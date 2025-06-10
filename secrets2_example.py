0731208a7fbf05cce24387fb69ee195d0929af6d
Vini <101851095+vnshot@users.noreply.github.com>
2025-06-10 07:55:57 -0300

    Create secrets2_example.py

---
+++ secrets2_example.py
@@ -0,0 +1,10 @@
+# Exemplo de arquivo de segredos para o monitor IPTV
+# NÃO coloque este arquivo em produção, use apenas como modelo!
+
+TELEGRAM_TOKEN = "coloque_seu_token_aqui"
+TELEGRAM_CHAT_ID = "coloque_seu_chat_id_aqui"
+SERVIDORES = [
+    # Exemplo:
+    # {"nome": "Servidor 1", "url": "http://exemplo.com/playlist.m3u8"}
+]
+SENHA = "sua_senha_aqui"
