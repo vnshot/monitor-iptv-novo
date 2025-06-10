41dd3c2dfdbde93e5739af5206dfb7db9c6621e5
Vini <101851095+vnshot@users.noreply.github.com>
2025-06-10 07:59:49 -0300

    Create secrets2.py

---
+++ secrets2.py
@@ -0,0 +1,17 @@
+# secrets2.py - NÃO subir em repositório público!
+
+TELEGRAM_TOKEN = "7287184604:AAG8glrBpENXzbsEEjcl4pJMAndfO3WDEdc"
+TELEGRAM_CHAT_ID = "@silvas89knba"
+
+# Dicionário de servidores: nome -> url
+SERVIDOR_URLS = {
+    "ninety": "http://dns.phaptom.shop/get.php?username=58804112&password=50872250&type=m3u_plus&output=mpegts",
+    "uniplay": "http://venombite.top/get.php?username=303863753&password=D144S6660c&type=m3u_plus&output=hls",
+    "elite": "http://ufcc.asia/get.php?username=vinagikev&password=PX56AW7X2AP8&type=m3u_plus&output=ts",
+    "club": "http://xwkhb.info/get.php?username=TccDP3zF&password=Xw7N7e&type=m3u_plus&output=ts",
+    "five": "http://cdn4k.live/get.php?username=kjagayan&password=k96vjavkm&type=m3u_plus&output=ts",
+    "now": "http://l.m3u4.xyz/3sfj35gj2"
+}
+
+# Senha de acesso ao painel (altere para uma senha forte)
+SENHA = "iptv2024"
